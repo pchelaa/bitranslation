7d4930d5633d107868d6a57cb13397aaa5ec9edc
Andrey Kirkov <kirkov.andrey@gmail.com>
2020-05-09 17:36:27 +0300

    Reverted back before max pooling

--- fairseq/fairseq/models/transformer_flow.py
+++ fairseq/fairseq/models/transformer_flow.py
@@ -858,8 +858,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 nsamples=1
             )
 
-        z = torch.max(z, dim=1)[0].unsqueeze(1).transpose(0, 1)
-        x += z
+        x += z.transpose(0, 1)
 
         self_attn_padding_mask: Optional[Tensor] = None
         if self.cross_self_attention or prev_output_tokens.eq(self.padding_idx).any():
