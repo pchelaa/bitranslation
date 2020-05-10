90eb27fc76dbd6edce0a7bfdbae2d46c1396a2a9
Andrey Kirkov <kirkov.andrey@gmail.com>
2020-05-10 15:56:27 +0300

    Bidirectional transformer: fix bug

--- fairseq/fairseq/data/language_pair_dataset.py
+++ fairseq/fairseq/data/language_pair_dataset.py
@@ -264,7 +264,7 @@ class LanguagePairDataset(FairseqDataset):
         return max(self.src_sizes[index], self.tgt_sizes[index] if self.tgt_sizes is not None else 0)
 
     def first_token(self, index):
-        return self.src[index][0]
+        return self.src[index][-1]
 
     def size(self, index):
         """Return an example's size as a float or tuple. This value is used when
